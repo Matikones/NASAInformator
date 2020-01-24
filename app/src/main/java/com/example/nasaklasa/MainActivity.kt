79182544294d4e72ed4ff937c68437234ae51b10
Matikones <matiadamek97@gmail.com>
2020-01-24 22:53:33 +0100

    Add left side menu

--- app/src/main/java/com/example/nasaklasa/MainActivity.kt
+++ app/src/main/java/com/example/nasaklasa/MainActivity.kt
@@ -9,6 +9,10 @@ import android.util.Log
 import android.view.*
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nasaklasa.ui.main.Earth
@@ -22,20 +26,33 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import com.example.nasaklasa.ui.main.MainViewModel
 import com.example.nasaklasa.ui.main.Save
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         updateUI()
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.naw_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, 0, 0
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_details, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.act0){
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment())
@@ -43,8 +60,8 @@ class MainActivity : AppCompatActivity() {
         }
         if (item.itemId == R.id.act1){
             supportFragmentManager.beginTransaction()
-            .replace(R.id.container, Earth())
-            .commitNow()
+                .replace(R.id.container, Earth())
+                .commitNow()
         }
         if (item.itemId == R.id.act2){
             Toast.makeText(this, "funkcja Mateusza ", Toast.LENGTH_LONG).show()
@@ -66,6 +83,7 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
     private fun updateUI(){
         val fm = supportFragmentManager
         var fragment = fm.findFragmentById(R.id.container)
