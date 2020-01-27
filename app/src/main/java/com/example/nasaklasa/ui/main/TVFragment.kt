81c3bb4e32b8c7659a37da0d3bdd2860f1cf059e
Filiponesco <fifigaw98@gmail.com>
2020-01-27 16:36:24 +0100

    add progressbar to NASA TV

--- app/src/main/java/com/example/nasaklasa/ui/main/TVFragment.kt
+++ app/src/main/java/com/example/nasaklasa/ui/main/TVFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ProgressBar
 import androidx.lifecycle.ViewModelProviders
 
 import com.example.nasaklasa.R
@@ -18,6 +20,9 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.Abs
 class TVFragment : Fragment() {
     lateinit var mainViewModel: MainViewModel
     lateinit var youTubePlayerView: YouTubePlayerView
+
+    lateinit var progressBar: ProgressBar
+    lateinit var container: FrameLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,6 +39,11 @@ class TVFragment : Fragment() {
         }
 
         youTubePlayerView = view!!.findViewById(R.id.youtube_player_view)
+
+        progressBar = activity!!.findViewById(R.id.progressBar)
+        container = activity!!.findViewById(R.id.container)
+        container.visibility = View.INVISIBLE
+        progressBar.visibility = View.VISIBLE
         initYouTubePlayerView()
     }
     private fun initYouTubePlayerView() {
@@ -42,6 +52,9 @@ class TVFragment : Fragment() {
             override fun onReady(youTubePlayer: YouTubePlayer) {
                 val videoId = "21X5lGlDOfg"
                 youTubePlayer.loadVideo(videoId, 0f)
+
+                progressBar.visibility = View.GONE
+                container.visibility = View.VISIBLE
             }
         })
     }
