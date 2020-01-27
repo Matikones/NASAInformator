3e2e56570614930ce067175972e6bfd16d49d743
Arkadiusz Datka <Arkadiusz_Datka@outlook.com>
2020-01-27 21:17:59 +0100

    zadanie 2 z implementacji

--- app/src/main/java/com/example/nasaklasa/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/nasaklasa/ui/main/MainFragment.kt
@@ -1,8 +1,11 @@
 package com.example.nasaklasa.ui.main
 
+import android.content.Intent
+import android.net.Uri
 import android.opengl.Visibility
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -21,7 +24,6 @@ import kotlinx.android.synthetic.main.main_fragment.Url_row
 class MainFragment : Fragment() {
 
     lateinit var mainViewModel : MainViewModel
-
     private lateinit var viewModel: MainViewModel
 
     lateinit var progressBar: ProgressBar
@@ -61,6 +63,14 @@ class MainFragment : Fragment() {
                     .placeholder(R.mipmap.ic_nasa_foreground)
                     .into(ivImageMain_row)
 
+                //Zadanie 2
+                Url_row.setOnClickListener{
+                    Log.e("lolo","okoko")
+                    val openURL = Intent(Intent.ACTION_VIEW)
+                    openURL.data = Uri.parse(imageUrl)
+                    startActivity(openURL)
+                }
+                //Koniec zadania 2
                 progressBar.visibility = View.GONE
                 container.visibility = View.VISIBLE
             }
